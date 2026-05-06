2889378615ba71da2a53b9d8ae985a9cc9412691
nom1 <nom1@email.com>
2026-05-06 16:57:10 +0200

    Emprunt

--- src/Emprunt.java
+++ src/Emprunt.java
@@ -1,2 +1,31 @@
+import java.sql.Date;
+import java.time.LocalDate;
+
 public class Emprunt {
+    private Client client;
+    private Exemplaire exemplaire;
+    private Date dateEmprunt;
+    private Date dateRetourPrevue;
+
+    public Emprunt(Client client, Exemplaire exemplaire) {
+        this.client = client;
+        this.exemplaire = exemplaire;
+        this.dateEmprunt = Date.valueOf(LocalDate.now());
+        this.dateRetourPrevue = Date.valueOf(LocalDate.now().plusDays(14));
+    }
+
+    public Exemplaire getExemplaire() {
+        return exemplaire;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    @Override
+    public String toString() {
+        return "Emprunt : " + exemplaire.getLivre().getTitre() + 
+               " par " + client.getNom() + 
+               " (Retour prévu le : " + dateRetourPrevue + ")";
+    }
 }
