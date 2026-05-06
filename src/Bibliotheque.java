e78486397b67b67879d778460d3232d51476ba87
Vanvans04 <ivanntherin04@gmail.com>
2026-05-06 17:14:38 +0200

    Début implémentation bibliothèque

--- src/Bibliotheque.java
+++ src/Bibliotheque.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Bibliotheque {
+    private static Bibliotheque bibliotheque;
+    private ArrayList<Client> clients;
+    private Gestionnaire gestionnaire;
+
+    private Bibliotheque() {
+        clients = new ArrayList<>();
+    }
+
+    public static Bibliotheque getBibliotheque() {
+        if (bibliotheque == null) {
+            bibliotheque = new Bibliotheque();
+        }
+        return bibliotheque;
+    }
+
+    public void ajouterClient(Client client) {
+        clients.add(client);
+    }
+
+    public void supprimerClient(Client client) {
+        clients.remove(client);
+    }
 }
