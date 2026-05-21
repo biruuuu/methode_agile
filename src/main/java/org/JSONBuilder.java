ca4a6e1235f211acfc79712c1c39d6210f50d3be
arthurprnt <arthur.perronnet@gmail.com>
2026-05-21 12:09:15 +0200

    ajout meth db

--- src/main/java/org/JSONBuilder.java
+++ src/main/java/org/JSONBuilder.java
@@ -1,8 +1,10 @@
 package org;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 import javax.xml.crypto.Data;
+import java.io.File;
 import java.io.InputStream;
 import java.net.Socket;
 
@@ -37,18 +39,78 @@ public class JSONBuilder {
         }
     }
 
+    public void writeDb() {
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            // indent du json
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+            String cleanPath = this.path.startsWith("/") ? this.path.substring(1) : this.path;
+            File file = new File("src/main/resources/" + cleanPath);
+
+            mapper.writeValue(file, this.db);
+
+            System.out.println("Base de données sauvegardée avec succès dans " + file.getAbsolutePath());
+
+        } catch (Exception e) {
+            System.out.println("Erreur lors de la sauvegarde du JSON : ");
+            e.printStackTrace();
+        }
+    }
+
     public void updateCompte(int id, Compte newCompte) {
         Compte compteAModif = db.comptes.stream()
-                .filter(c -> c.getId() == id) // Remplace getId() par le bon nom de ton getter d'ID
+                .filter(c -> c.getId() == id)
                 .findFirst()
                 .orElse(null);
 
         if(compteAModif != null) {
             int ind = db.comptes.indexOf(compteAModif);
             db.comptes.set(ind, newCompte);
-            System.out.printf("Compte mis à jour avec succès!");
+            System.out.printf("Compte mis à jour avec succès!\n");
         } else {
-            System.out.printf("Erreur lors de la mise à jour");
+            db.comptes.add(newCompte);
         }
+    }
+
+    public void addCompte(Compte newCompte) {
+        updateCompte(-1, newCompte);
+    }
+
+    public void updateLivre(int isbn, Livre newLivre) {
+        Livre livreAModif = db.livres.stream()
+                .filter(l -> l.getIsbn() == isbn)
+                .findFirst()
+                .orElse(null);
+
+        if(livreAModif != null) {
+            int ind = db.livres.indexOf(livreAModif);
+            System.out.printf("Livre mis à jour avec succès!\n");
+        } else {
+            db.livres.add(newLivre);
+        }
+    }
+
+    public void addLivre(Livre newLivre) {
+       updateLivre(-1, newLivre); 
+    }
+
+    public void updateExemplaire(int id, Exemplaire newEx) {
+      Exemplaire exAModif = db.exemplaires.stream()
+        .filter(e -> e.getId() == id)
+        .findFirst()
+        .orElse(null);
+
+      if(exAModif != null) {
+        int ind = db.exemplaires.indexOf(exAModif);
+        System.out.println("Exemplaire mis a jour avec succès!");
+      } else {
+        db.exemplaires.add(newEx);
+      }
+    } 
+
+    public void addExemplaire(Exemplaire newExemplaire) {
+      updateExemplaire(-1, newExemplaire);
     }
 }
